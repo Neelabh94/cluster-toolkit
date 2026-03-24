12cca0ae8e8e3b9db49c1c67806ae44fd1964ce9
Ivan Sikirić <isikiric@google.com>
2026-03-24 15:52:01 +0100

    revert unmerged changes

--- modules/scheduler/gke-cluster/main.tf
+++ modules/scheduler/gke-cluster/main.tf
@@ -55,8 +55,7 @@ locals {
     "STORAGE",
     "HPA",
     "CADVISOR",
-    "KUBELET",
-    "JOBSET"
+    "KUBELET"
   ]
 
   default_logging_component = [
@@ -244,9 +243,6 @@ resource "google_container_cluster" "gke_cluster" {
         disabled = false
       }
     }
-    slice_controller_config {
-      enabled = var.enable_slice_controller
-    }
   }
 
   timeouts {
@@ -284,13 +280,6 @@ resource "google_container_cluster" "gke_cluster" {
     precondition {
       condition     = coalesce(var.enable_multi_networking, true) || length(var.additional_networks) == 0
       error_message = "'enable_multi_networking' cannot be false when using multivpc module, which passes additional_networks."
-    }
-    precondition {
-      condition = (
-        !var.enable_slice_controller ||
-        try(tonumber(split(".", local.master_version)[0]) > 1 || (tonumber(split(".", local.master_version)[0]) == 1 && tonumber(split(".", local.master_version)[1]) >= 35), true)
-      )
-      error_message = "The GKE Slice Controller requires a GKE version of 1.35 or higher. Please update 'version_prefix' or 'min_master_version'."
     }
   }
 

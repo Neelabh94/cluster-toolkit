34643388b0cb6e8da36e81f6f46429348caaa3bb
Neelabh94 <neelgoyal@google.com>
2026-07-23 08:44:33 +0000

    fix(job): fail when --mount-options used without GCS mounts

--- cmd/job/submit.go
+++ cmd/job/submit.go
@@ -388,6 +388,9 @@ func validateGKENAPFlags() error {
 
 func validateMountOptions() error {
 	if mountOptions != "" {
+		if len(volumeStr) == 0 {
+			return fmt.Errorf("--mount-options requires at least one GCS mount")
+		}
 		for _, m := range volumeStr {
 			if !strings.HasPrefix(m, "gs://") {
 				return fmt.Errorf("--mount-options is currently only supported for GCS fuse volumes (gs://...)")
